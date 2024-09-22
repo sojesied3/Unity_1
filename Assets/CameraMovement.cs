1ef0c95da141d21831994cfc1625603e1e0344c3
unknown <dr.till007@gmail.com>
2024-09-22 09:15:16 +0700

    add int i and comment test

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -7,7 +7,7 @@ public class CameraMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int i = 0;
     }
 
     // Update is called once per frame
@@ -15,4 +15,5 @@ public class CameraMovement : MonoBehaviour
     {
         transform.position = player.position + offset;
     }
+    //Test
 }
