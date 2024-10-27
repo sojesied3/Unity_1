d7f419792c528900841870ecda85a35245f84e43
Charles <Charlestemporary30@gmail.com>
2024-10-27 11:15:18 +0700

    Added Particles and Coin Idle Animation

--- Assets/CoinHitbox.cs
+++ Assets/CoinHitbox.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 
 public class CoinHitbox : MonoBehaviour
 {
+    public GameObject MultipleSlash;
+    bool isUpward;
+    bool isDownward;
+    void CoinStageUp()
+    {
+        this.transform.Translate(0, 0.2f, 0);
+    }
+    void CoinStageDown()
+    {
+        this.transform.Translate(0, -0.2f, 0);
+    }
     CarMovement a;
     private void OnCollisionEnter(Collision collision)
     {
@@ -14,17 +25,41 @@ public class CoinHitbox : MonoBehaviour
             a = collision.gameObject.GetComponentInParent<CarMovement>();
             a.AddCoin();
             this.gameObject.SetActive(false);
+            //MultipleSlash.SetActive(false);
         }
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        isUpward = true;
+        isDownward = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //MultipleSlash.SetActive(true);
         this.transform.Rotate(0, 0.2f, 0);
+        //this.transform.Translate(0, 0.2f, 0);
+        if (this.transform.position.y > 5.5)
+        {
+            isDownward = true;
+            isUpward = false;
+        }
+        if (this.transform.position.y < 4.5)
+        {
+            isDownward = false;
+            isUpward = true;
+        }
+
+        if (isUpward == true)
+        {
+            this.transform.Translate(0, 0.002f, 0);
+        }
+        if (isDownward == true)
+        {
+            this.transform.Translate(0, -0.002f, 0);
+        }
     }
 }
+
